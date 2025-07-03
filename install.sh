6cc47eebfc58c6d82deac24dbf3a1cc84a5e00e8
stevia <pt8786995@gmail.com>
2025-07-04 02:48:31 +0300

    v1.0

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/bash/
 
-sudo apt install thunar fish fastfetch wofi waybar wayland qpwgraph kitty hyprlock hyprpaper hyprland firefox code
-#sudo pacman -S thunar fish fastfetch wofi waybar wayland qpwgraph kitty hyprlock hyprpaper hyprland firefox code
+sudo apt install thunar fish fastfetch wofi waybar wayland qpwgraph kitty hyprlock hyprpaper hyprland hyprshot firefox code
+#sudo pacman -S thunar fish fastfetch wofi waybar wayland qpwgraph kitty hyprlock hyprpaper hyprland hyprshot firefox code
 cp -r ./hypr/* ~/.config/hypr/
 cp ./kitty/* ~/.config/kitty/
 cp ./waybar/* ~/.config/waybar/
